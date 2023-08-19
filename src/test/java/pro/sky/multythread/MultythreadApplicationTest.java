4515de2ba9b032335975cc2dbc4eff9781d16668
arxip <arxipov1989@gmail.com>
2023-08-19 12:21:20 +0300

    3.6

--- src/test/java/pro/sky/multythread/MultythreadApplicationTest.java
+++ src/test/java/pro/sky/multythread/MultythreadApplicationTest.java
@@ -1,11 +1,26 @@
 package pro.sky.multythread;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.multythread.controller.AvatarController;
+import pro.sky.multythread.controller.FacultyController;
+import pro.sky.multythread.controller.StudentController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MultythreadApplicationTest {
+    @Autowired
+    private StudentController studentController;
+    @Autowired
+    private FacultyController facultyController;
+    @Autowired
+    private AvatarController avatarController;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(studentController);
+        Assertions.assertNotNull(facultyController);
+        Assertions.assertNotNull(avatarController);
     }
 }
