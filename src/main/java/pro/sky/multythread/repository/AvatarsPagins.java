c7054e38efda57982c310b286f3270c8a3178160
arxip <arxipov1989@gmail.com>
2023-09-02 21:14:22 +0300

    3.6

--- src/main/java/pro/sky/multythread/repository/AvatarsPagins.java
+++ src/main/java/pro/sky/multythread/repository/AvatarsPagins.java
@@ -2,12 +2,8 @@ package pro.sky.multythread.repository;
 
 public interface AvatarsPagins {
     Long getId();
-
     String getPath();
-
     Integer getSize();
-
     String getType();
-
     Long getStudentId();
 }
